1bd2debfc6d8470c7b7896e33683b706573d78b7
neelam64 <neelam.mangnani@cumminscollege.in>
2024-06-19 20:10:56 +0530

    g

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   const value = 'World';
   return <div>
     <h1>Hello {value}</h1>
-    Hello {value}</div>;
+   <h2>This is my React Static web Application</h2>
+   <p> My name is Neelam Mangnani</p>
+   </div>;
 }
 
 export default App;
