efd55e8fbfd304a89487a93ec5a4cc5ec0a75c47
neelam64 <neelam.mangnani@cumminscollege.in>
2024-06-19 20:09:06 +0530

    jk

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>
+    <h1>Hello {value}</h1>
+    Hello {value}</div>;
 }
 
 export default App;
